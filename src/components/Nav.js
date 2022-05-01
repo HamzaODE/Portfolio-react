441950d2c8c7f575650e3aaf34dc58e27bea513f
= <hamza.mehboob@eyzinsure.com>
2022-05-01 07:47:52 +0500

    Assets added

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Nav extends Component {
+    static propTypes = {}
+
+    render() {
+        return (
+            <div>Nav</div>
+        )
+    }
+}
+
+export default Nav
